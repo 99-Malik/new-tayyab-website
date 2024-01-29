655a798bf341cf2a63dbdad5117bde4d135ba02c
Usama Iftikhar <usamaiftikhar718@gmail.com>
2024-01-29 09:31:23 +0500

    gtag added

--- app/layout.jsx
+++ app/layout.jsx
@@ -23,52 +23,33 @@ export const metadata = {
 
 // Main layout component
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <Head>
-        {/* Other head elements... */}
-        {/* Google Tag Manager - Global base code */}
-        <Script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-5X9MZ6FF');
-            `
-          }}
-        />
-        <Script
-          defer
-          src="https://pulse.clickguard.com/s/accAKdAZtP5Vl/astWa776s9CIj"
-        />
-        {/* Google tag (gtag.js) */}
-        <Script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=GT-KFTQ5FS"
-        />
-        <Script
-          dangerouslySetInnerHTML={{
-            __html: `
+  return <html lang="en">
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=GT-KFTQ5FS" />
+      <Script dangerouslySetInnerHTML={{ __html: `
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
 
               gtag('config', 'GT-KFTQ5FS');
-            `
-          }}
-        />
+            ` }} />
+
+      <Head>
+        {/* Other head elements... */}
+        {/* Google Tag Manager - Global base code */}
+        <Script dangerouslySetInnerHTML={{ __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-5X9MZ6FF');
+            ` }} />
+        <Script defer src="https://pulse.clickguard.com/s/accAKdAZtP5Vl/astWa776s9CIj" />
+        {/* Google tag (gtag.js) */}
         {/* Other head scripts... */}
       </Head>
       <body className={inter.className}>
         <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-5X9MZ6FF"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          />
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5X9MZ6FF" height="0" width="0" style={{ display: "none", visibility: "hidden" }} />
         </noscript>
         <Header className="mt-[-10px]" />
         {children}
@@ -78,6 +59,5 @@ export default function RootLayout({ children }) {
         </Suspense>
         <Footer />
       </body>
-    </html>
-  );
+    </html>;
 }
